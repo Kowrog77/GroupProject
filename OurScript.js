7cd793a670b4f783446beb7dccf2a05f9a3cdd52
Ming Cường Nguyễn Đỗ <ngdm.cuong@gmail.com>
2019-03-02 13:21:55 -0600

    put things in columns

--- OurScript.js
+++ OurScript.js
@@ -1,81 +1,64 @@
-var pizType;
-let pizNum;
-let prize =10;
-let tax = 0.076;
+let piztype;
+let qty;
+let qty2;
 
-let pizArray = ["Pepperoni", "Koen's Famous Olive", "Cheese", "Canadian Bacon", "Fish Sauce"];
+pizArray = ["Pepperoni", "Koen's Famous Olive", "Cheese", "Canadian Bacon", "Fish Sauce"];
+
 
 function addPizza() {
     taskList = document.getElementById('piz');
-    for (var i = 0; i < pizArray.length; i++) {
-        taskList.innerHTML += '<option value="' + i + '" id="piz' + i + '">' + pizArray[i] + '</option>';
-
-        // taskList.innerHTML += "<option  value = \"" +pizArray[i]+ "\">" + pizArray[i] + "</option>";
+    for (var i = 0; i < (pizArray.length); i++) {
+        taskList.innerHTML += "<option value = \"" +pizArray[i]+ "\">" + pizArray[i] + "</option>";
     }
 }
 
 function addNum() {
-    var taskList;
     taskList = document.getElementById('qty');
-    for (var j = 0; j < 10; j++) {
-        taskList.innerHTML += '<option value="' +  j  + '" id="num' + (j +1) + '">' +  (j + 1) + '</option>';
-        // taskList.innerHTML += "<option value = \"" +  (i + 1) + "\">" + (i+1) + "</option>" ;
+    for (var i = 0; i < 10; i++) {
+        taskList.innerHTML += "<option value = \"" +  (i + 1) + "\">" + (i+1) + "</option>" ;
     }
 }
-
-function addStats() {
-    taskList = "";
-    taskList = document.getElementById('summary');
-
-}
-
-
-function myFunction(element) {
-    pizType=element;
-       
-    }
-
-    // This is to test if the pizType takes the value or not
-console.log(pizType);
-
-
 
 addPizza();
 addNum();
-// document.getElementById("submit").addEventListener("click", getPizza());
+
+function getType(piztype) {
+    let element1 = document.getElementById("pizzaType");
+    element1.value = piztype;
+    element1.innerHTML = piztype;
+}
+function getQty(qty) {
+    let element2 = document.getElementById("Quantity");
+    element2.value = qty;
+    element2.innerHTML = qty;
+	qty2 = qty;
+}
+
+//let name = "Kirsten Markley";
+//let Tel = "417 123 4567";
+let price = 10;
 
 
-// for (let i = 0; i < pizArray.length; i++) {
-//     document.getElementById("piz" + i).addEventListener("click", getPizza.bind(this, i));
-// }
 
-// // for (let i = 1; i <= 10; i++) {
-// //     document.getElementById("num" + i).addEventListener("click", getNum.bind(this, i));
-// // }
+//qty = getElementById(''); // get qty value
 
 
 
 
 
+function displayOut() {
+    document.getElementById('name').innerHTML = 'Kirsten Markley';
+    document.getElementById('phone').innerHTML = '417 123 4567';
+	let tax = 0.076;
+	out1 = document.getElementById("SubTotal");
+	let subtotal = (qty2 * 10);
+	out1.innerHTML = ("SubTotal: $" + subtotal);
+	out2 = document.getElementById("Tax");
+	let taxTotal = (subtotal * tax);
+	out2.innerHTML = ("Tax: $" + taxTotal.toFixed(2));
+	out3 = document.getElementById("Total");
+	let total = (subtotal * taxTotal);
+	out3.innerHTML = ("Total: $" + total.toFixed(2));
+}
 
-// function getType(piztype) {
-//     var element = document.getElementById("pizzaType");
-//     element.value = piztype;
-//     element.innerHTML = piztype;
-// }
-// let quantity;
-
-// function getQty(qty) {
-//     var element = document.getElementById("Quantity");
-//     element.value = qty;
-//     element.innerHTML = qty;
-//     quantity=Number(qty);
-// }
-
-// let stotal = quantity *  prize;
-// let total = stotal*tax;
-// var asdasd;
-// asdasd = document.getElementById('SubTotal');
-
-// asdasd.addEventListener("click",document.getElementById('SubTotal'));
-// console.log(`Total :${stotal}`);
+document.getElementById("submit").addEventListener("click", displayOut); // Event listener when clicking order button
