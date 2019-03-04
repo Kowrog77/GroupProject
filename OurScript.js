0ca51ea50f3ac11198d2dbab85d234cfe53f81bf
GHAN <jg0885758@otc.edu>
2019-03-04 16:30:18 -0600

    final

--- OurScript.js
+++ OurScript.js
@@ -1,66 +1,52 @@
 let piztype;
 let qty;
-let qty2;
 
-pizArray = ["Pepperoni", "Koen's Famous Olive", "Cheese", "Canadian Bacon", "Fish Sauce"];
+
+pizArray = ["Pepperoni", "Olive", "Cheese", "Bacon", "Ham"];
 
 
 function addPizza() {
     taskList = document.getElementById('piz');
     for (var i = 0; i < (pizArray.length); i++) {
-        taskList.innerHTML += "<option value = \"" +pizArray[i]+ "\">" + pizArray[i] + "</option>";
+        taskList.innerHTML += "<option value = "+ pizArray[i] +">" + pizArray[i] + "</option>";
     }
 }
 
 function addNum() {
     taskList = document.getElementById('qty');
     for (var i = 0; i < 10; i++) {
-        taskList.innerHTML += "<option value = \"" +  (i + 1) + "\">" + (i+1) + "</option>" ;
+        taskList.innerHTML += "<option value = " +  (i + 1) + ">" + (i+1) + "</option>" ;
     }
 }
 
 addPizza();
 addNum();
 
-function getType(piztype) {
-    let element1 = document.getElementById("pizzaType");
-    element1.value = piztype;
-    element1.innerHTML = piztype;
-}
-function getQty(qty) {
-    let element2 = document.getElementById("Quantity");
-    element2.value = qty;
-    element2.innerHTML = qty;
-	qty2 = qty;
-}
-
-//let name = "Kirsten Markley";
-//let Tel = "417 123 4567";
+let name = "Kirsten Markley";
+let Tel = "417-123-4567";
 let price = 10;
 
 
 
-//qty = getElementById(''); // get qty value
-
-
-
-
-
 function displayOut() {
-	thank = document.getElementById("Thankyou");
-	thank.innerHTML = ("Thank You!");
+    pizzaType = document.getElementById("piz");
+    pizzaType = pizzaType.value;
+    qty = document.getElementById("qty");
+    qty = qty.value;
+   document.getElementById("summary").innerHTML = ('<br><h3 id ="Thankyou"></h3><br> <div>Order Info:</div><br> <div class="row"> <div class="col-sm"> <p>Name: </p> </div> <div class="col-sm"> <p>' + name + '</p> </div> </div> <div class="row"> <div class="col-sm"> <p>Phone Number: </p> </div> <div class="col-sm"> <p>' + Tel + '</p> </div> </div> <div class="row"> <div class="col-sm"> <p>Pizza Type: </p> </div> <div class="col-sm"> <p id="pizzaType"></p> </div> </div> <div class="row"> <div class="col-sm"> <p>Quantity: </p> </div> <div class="col-sm"> <p id= "qty2"></p> </div> </div> <div class="row"> <div class="col-sm"> <p>Subtotal: </p> </div> <div class="col-sm"> <p id="SubTotal"></p> </div> </div> <div class="row"> <div class="col-sm"> <p>Tax: </p> </div> <div class="col-sm"> <p id ="Tax"></p> </div> </div> <div class="row"> <div class="col-sm"> <p>Total: </p> </div> <div class="col-sm"> <p id ="Total"></p> </div>');
+
+    document.getElementById("Thankyou").innerHTML = ("Thank You!");
+    document.getElementById("pizzaType").innerHTML = (pizzaType);
+    document.getElementById("qty2").innerHTML = (qty);
 	let tax = 0.076;
-	out1 = document.getElementById("SubTotal");
-	let subtotal = (qty2 * 10);
-	out1.innerHTML = ("$" + subtotal);
-	out2 = document.getElementById("Tax");
+    let subtotal = (qty * 10);
+    document.getElementById("SubTotal").innerHTML = ("$" + subtotal);
 	let taxTotal = (subtotal * tax);
 	let taxTotal2 = taxTotal.toFixed(2);
-	out2.innerHTML = ("$" + taxTotal2);
-	out3 = document.getElementById("Total");
+	document.getElementById("Tax").innerHTML = ("$" + taxTotal2);
 	let total = (subtotal + taxTotal);
 	total = total.toFixed(2);	 
-	out3.innerHTML = ("$" + total);
+	document.getElementById("Total").innerHTML = ("$" + total);
 }
 
 document.getElementById("submit").addEventListener("click", displayOut); // Event listener when clicking order button
